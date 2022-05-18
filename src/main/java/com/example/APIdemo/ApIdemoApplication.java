68b1445eb60366df7ec0bc22f53d8f248e2f74c6
Yen <vunganyen2000@gmail.com>
2022-05-18 21:41:46 +0700

    first

---
+++ src/main/java/com/example/APIdemo/ApIdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIdemoApplication.class, args);
+	}
+
+}
